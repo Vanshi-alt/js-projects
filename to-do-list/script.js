1cedc8c42d1ca48e336f9fb5abcd3afc96771bed
Vanshi-alt <vanshikas7788@gmail.com>
2025-09-04 16:02:54 +0000

    to-do-list

---
+++ to-do-list/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded' , () =>{
+    const todoInput = document.getElementById("tasktoenter");
+const addTaskList = document.getElementById("addtask");
+const list = document.getElementById("todolist");
+
+let tasks =JSON.parse(localStorage.getItem('tasks'))  || [] ;
+
+tasks.forEach(task => renderTasks(task) )
+
+
+addTaskList.addEventListener('click' , function(){
+    const taskText = todoInput.value.trim();
+    if(taskText === "") return;
+
+    const newTask = {
+    id: Date.now(),
+    text : taskText,
+    completed: false
+}
+
+tasks.push(newTask);
+saveTasks();
+todoInput.value = ""; //clearInput
+console.log(tasks);
+
+})
+
+function renderTasks(task){
+    console.log(task);
+}
+function saveTasks(){
+    localStorage.setItem('tasks' , JSON.stringify(tasks));
+}
+
+
+
+})
